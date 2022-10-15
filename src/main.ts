0280b5f52b72039ee7ca990550c8e3efcae0dd0e
eziopu <atkdet@gmail.com>
2022-10-15 21:21:29 +0800

    chore(package): rm test, pinia because pacaage don't use

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import App from "./App.vue";
 
 import "./assets/main.css";
 
-const app = createApp(App);
-
-app.use(createPinia());
-
-app.mount("#app");
+createApp(App).mount("#app");
