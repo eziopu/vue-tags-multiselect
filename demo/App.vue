08596327710032eb182c330cbc563a8c3904e78a
Eziopu <atkdet@gmail.com>
2023-05-12 18:27:22 +0800

    demo page index, i18n

--- demo/App.vue
+++ demo/App.vue
@@ -1,12 +1,142 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
+import { useI18n } from "vue-i18n";
+
+// import HelloWorld from "./components/HelloWorld.vue";
 import Play from "./components/play-helps/main.vue";
+
+const i18nLocale = useI18n();
+const frameworks = ["default", "bootstrap", "semantic-ui"];
+const theme = ref("default");
+
+/* theme change */
+const changeTheme = (framework = "") => {
+  location.href =
+    window.location.protocol +
+    "//" +
+    window.location.host +
+    (framework != "default" ? "?theme=" + framework : "");
+};
+
+/* set theme */
+const urlQueryTheme = new URL(location.href).searchParams.get("theme");
+if (
+  frameworks.find((framework) => {
+    return framework == urlQueryTheme;
+  })
+) {
+  theme.value = urlQueryTheme;
+}
 </script>
 
 <template>
-  <Play />
+  <link
+    v-if="theme == 'bootstrap'"
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+    crossorigin="anonymous"
+  />
+  <link
+    v-if="theme == 'semantic-ui'"
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+    integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ=="
+    crossorigin="anonymous"
+  />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+  />
 
-  <header>
+  <div class="ui container" :class="theme">
+    <div class="ui-frameworks">
+      <h2>{{ $t("ui.page.description.demo") }}</h2>
+      <ul>
+        <li>
+          <div class="framework">
+            <h4>UI framework:</h4>
+            <button
+              class="btn pointer"
+              v-for="(framework, index) in frameworks"
+              @click="changeTheme(framework)"
+              :key="`framework${index}`"
+              :class="{
+                active: theme == framework,
+                'btn-outline-primary': theme == 'bootstrap',
+                'ui primary button': theme == 'semantic-ui',
+                'basic ': theme == 'semantic-ui' && theme != framework,
+              }"
+            >
+              <span v-if="framework == 'default'">
+                {{ $t("ui.general.default") }}
+              </span>
+              <span v-else>{{ framework }}</span>
+              <span
+                class="version"
+                v-if="theme == framework && framework != 'default'"
+                v-html="framework == 'bootstrap' ? ' v4.6.0' : ' v2.4.1'"
+              >
+              </span>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <h4>icon: font-awesome v4.7.0</h4>
+        </li>
+
+        <li>
+          <div class="language">
+            <h4>language:</h4>
+            <select v-model="$i18n.locale">
+              <option
+                v-for="locale in i18nLocale.availableLocales"
+                :key="locale"
+                :value="locale"
+              >
+                {{ $t(`ui.languages.${locale}`) }}
+              </option>
+            </select>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+    <div class="demo" v-if="theme != 'default'">
+      <div class="demo-control">
+        <a
+          class="show-code-btn pointer"
+          style="color: inherit"
+          target="_blank"
+          :href="`https://github.com/eziopu/vue-tags-multiselect/blob/master/src/assets/stylesheets/${theme}.scss`"
+        >
+          scss <i class="fa fa-external-link"></i>
+        </a>
+      </div>
+    </div>
+    <hr />
+
+    <!-- <Keyboard></Keyboard>
+      <AppAttributes></AppAttributes>
+
+      <h3>Dropdown Slots Attributes</h3>
+      <DropdownAttributes></DropdownAttributes>
+
+      <h3>Option Slots Attributes</h3>
+      <OptionAttributes></OptionAttributes>
+
+      <h3>Other Slots</h3>
+      <OtherSlots></OtherSlots>
+
+      <h3>Custome style</h3>
+      <CustomStyle></CustomStyle>
+
+      <h3>Operate all Attributes</h3> -->
+    <Play :theme="theme" :originTableDatas="originTableDatas"></Play>
+  </div>
+
+  <!-- <header>
     <img
       alt="Vue logo"
       class="logo"
@@ -39,7 +169,7 @@ import Play from "./components/play-helps/main.vue";
         </v-tag-option>
       </v-tag-dropdown>
     </v-tags-multiselect>
-    <!-- <v-tag-dropdown value="country2" displayAll>
+    <v-tag-dropdown value="country2" displayAll>
       <v-tag-option title> title2222 </v-tag-option>
       <v-tag-option value="2Māre111" selected> 222Māre1111 </v-tag-option>
       <v-tag-option value="2Eldia2222" selected>
@@ -48,8 +178,9 @@ import Play from "./components/play-helps/main.vue";
       <v-tag-option value="22223333"> 22223333 </v-tag-option>
       <v-tag-option value="2224444"> 2224444 </v-tag-option>
       <v-tag-option value="22555"> 22555 </v-tag-option>
-    </v-tag-dropdown> -->
+    </v-tag-dropdown>
   </main>
+  -->
 </template>
 
 <style lang="scss" v-if="theme == 'bootstrap'">
