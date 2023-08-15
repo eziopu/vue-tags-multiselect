88eb704853b8cb7947c944ae88514670bae485f3
eziopu <atkdet@gmail.com>
2023-08-15 17:17:24 +0800

    use tag add log

--- src/composables/useTag.js
+++ src/composables/useTag.js
@@ -5,7 +5,7 @@ import {
   getTagsGroupByKeyValueModel,
 } from "../models";
 
-export default function useTag(_props, context) {
+export default function useTag(_props, context, dep) {
   // ============== DATA ==============
 
   const tags = reactive([]);
@@ -60,13 +60,22 @@ export default function useTag(_props, context) {
 
   // ============== METHODS ==============
 
+  const log = dep.log;
+  const log2 = dep.log2;
   const setStashTag = (item = {}) => {
     Object.assign(stashTag, { ...getTagModel(item) });
+    if (Object.keys(item).length === 0) {
+      log(`setStashTag`, `object clear`);
+    } else {
+      log(`setStashTag`, `to set`);
+      log2(`>`, item);
+    }
   };
 
   const setTagToTags = (item = {}) => {
     item.index = tags.length;
     tags.push({ ...getTagModel(item) });
+    log(`stashTag to tags`);
   };
 
   const updateTag = (item = {}) => {
@@ -79,6 +88,7 @@ export default function useTag(_props, context) {
     tag.value = item.value;
     tag.valueElm = item.valueElm || null;
     tag.displayValue = !!item.displayValue;
+    log(`update tag`, `success`);
   };
 
   const deleteTags = (indexs = []) => {
@@ -91,6 +101,7 @@ export default function useTag(_props, context) {
       // deleting or updating tag by index may affect the calculation of the index.
       delete tags[index];
       // tags.splice(index, 1);
+      log(`delete tag`, `index is ${index}`, `success`);
     }
   };
 
