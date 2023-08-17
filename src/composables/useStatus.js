6416436868bce7a5d808fe8d92c6125998c70ad4
eziopu <atkdet@gmail.com>
2023-08-17 18:33:02 +0800

    add log

--- src/composables/useStatus.js
+++ src/composables/useStatus.js
@@ -1,4 +1,4 @@
-import { computed } from "vue";
+import { computed, watch } from "vue";
 
 export default function useStatus(props, context, dep) {
   // ============== DATA ==============
@@ -16,17 +16,33 @@ export default function useStatus(props, context, dep) {
     const result = [];
     if (props.disabled == true) result.push("disabled");
     if (props.loading == true) result.push("loading");
-    if (isEditMode.value == true) result.push("editing");
+    if (isEditMode.value == true) {
+      dep.log(`edit the tag`);
+      result.push("editing");
+    }
     if (isElDropdownVisible.value == true) result.push("selecting");
-    if (appIsFinish.value == true) result.push("finish");
-    if (appIsFinish.value == false && isAllDropdownIsDown.value == true)
+    if (appIsFinish.value == true) {
+      dep.log(`app is done`);
+      result.push("finish");
+    }
+    if (appIsFinish.value == false && isAllDropdownIsDown.value == true) {
+      dep.log(`the options have been selected`);
       result.push("delect-down");
+    }
     return result;
   });
 
   // ============== EMIT ==============
 
   context.emit("status", status);
+
+  // ============== Watch all props to log ==============
+
+  if (dep.isAppActived == true) {
+    watch(props, (oldProps, newProps) => {
+      console.log(oldProps, newProps);
+    });
+  }
 
   return {
     status,
