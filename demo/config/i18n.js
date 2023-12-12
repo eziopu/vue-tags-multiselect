96ccddbefd641eebc8f4d1a187093dcc2c24e436
eziopu <atkdet@gmail.com>
2023-12-12 18:01:18 +0800

    chore i18n.ts to .js

--- demo/config/i18n.js
+++ demo/config/i18n.js
@@ -3,9 +3,7 @@ import { createI18n } from "vue-i18n";
 import enUS from "./locales/en-US.json";
 import zhTW from "./locales/zh-TW.json";
 
-type MessageSchema = typeof zhTW;
-
-const i18n = createI18n<[MessageSchema], "zh-TW" | "en-US">({
+const i18n = createI18n({
   legacy: false, // 要把 legacy 設為 false，才可以使用 Composition API
   locale: "zh-TW",
   fallbackLocale: "zh-TW",
