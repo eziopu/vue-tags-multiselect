fc3642ae4ef9dca8367a2124935919efac31cb4e
eziopu <atkdet@gmail.com>
2024-03-11 16:55:57 +0800

    refactor add demo package props model

--- demo/components/models.js
+++ demo/components/models.js
@@ -72,3 +72,73 @@ export const DATAS = [
     remark: "",
   },
 ];
+
+export const PACKAGE_PROPS = {
+  disabled: false,
+  loading: false,
+  search: true,
+  transition: true,
+  create: true,
+  merge: true,
+  debugLog: false,
+  conjunction: "",
+  deleteIcon: "always",
+  placeholder: "",
+  placeholders: {
+    initial: "",
+    loading: "Wait a moment, please.",
+    selectDown: "Selected End.",
+    finish: "Finish.",
+    tagValueRepeat: "",
+  },
+  dropdownLoading: false,
+  tagConjunction: "",
+  loadingContent: "",
+  dropdownLoadingContent: "",
+  optionUndoContent: "",
+  optionORConjunctionContent: "",
+}
+
+export const PACKAGE_EVENT = {
+  result: [],
+  status: [],
+  selectingTag: {},
+  inputValue: "",
+  editing: {},
+}
+
+export const PACKAGE_V_DROPDOWN_PROPS = {
+  isDisplayForDemo: false, // for show code
+  disabled: false,
+  divided: false,
+  displayAll: false,
+  hidden: false,
+  custom: false,
+}
+
+export const PACKAGE_V_OPTION_PROPS = {
+  isDisplayForDemo: false, // for show code
+  displayValue: false,
+  disabled: false,
+  divided: false,
+  selected: false,
+}
+
+export const DEMO_SETTING = {
+  props: PACKAGE_PROPS,
+  event: PACKAGE_EVENT,
+  option: {
+    country: generate_v_options(3),
+    name: generate_v_options(4),
+    remark: generate_v_options(3),
+  },
+  dropdown: {
+    country: PACKAGE_V_DROPDOWN_PROPS,
+    name: PACKAGE_V_DROPDOWN_PROPS,
+    remark: PACKAGE_V_DROPDOWN_PROPS,
+  },
+}
+
+function generate_v_options(number) {
+  return Array.from({ length: number }, () => ({ ...PACKAGE_V_OPTION_PROPS }));
+}
