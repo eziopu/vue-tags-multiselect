31e50c20582166cc4a2dc22114f26874e82758c4
eziopu <atkdet@gmail.com>
2024-03-11 20:27:02 +0800

    refactoe app merge demo

--- demo/components/app/merge.vue
+++ demo/components/app/merge.vue
@@ -12,8 +12,8 @@
 
     <GeneralDemo
       :displayRefreshBtn="true"
-      :selecteds="{
-        country: [true, true],
+      :option="{
+        country: [{}, {selected: true}, {selected: true}],
       }"
       :app="{
         merge: merge == 'true' ? true : false,
@@ -47,33 +47,5 @@ export default {
 }
 .attribute {
   margin-bottom: 6px;
-}
-@media all and (max-width: 768px) {
-  .flex-between.text {
-    display: block !important;
-  }
-}
-@media all and (max-width: 992px) {
-  .attributes {
-    flex-wrap: wrap;
-  }
-  .attribute {
-    &:nth-child(1) {
-      width: 50%;
-    }
-    &:nth-child(2) {
-      width: 50%;
-    }
-  }
-}
-@media all and (max-width: 470px) {
-  .attribute {
-    &:nth-child(1) {
-      width: 100%;
-    }
-    &:nth-child(2) {
-      width: 100%;
-    }
-  }
 }
 </style>
