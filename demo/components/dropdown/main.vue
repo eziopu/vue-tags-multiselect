ca685026e49af211617051bbe9bf59276f6d1413
eziopu <atkdet@gmail.com>
2024-03-13 17:53:16 +0800

    refctor 切割出other

--- demo/components/dropdown/main.vue
+++ demo/components/dropdown/main.vue
@@ -2,149 +2,22 @@
   <div id="dropdown-slot" class="demo">
     <Value />
     <Custom />
-
-    <div class="flex-between">
-      <div class="attributes">
-        <div class="attribute">
-          <h3>DisplayAll</h3>
-          <div class="flex-between">
-            <div class="depiction">
-              {{ $t("attributes.description.dropdown.display-all") }}
-            </div>
-            <LabelAndSelect
-              label=""
-              v-model="displayAll"
-              :disabled="hidden == 'true'"
-            />
-          </div>
-        </div>
-        <div class="attribute">
-          <h3>Disabled</h3>
-          <div class="flex-between">
-            <div class="depiction">
-              {{ $t("attributes.description.dropdown.disabled") }}
-            </div>
-            <LabelAndSelect
-              label=""
-              :disabled="hidden == 'true'"
-              v-model="disabled"
-            />
-          </div>
-        </div>
-        <div class="attribute">
-          <h3>Divided</h3>
-          <div class="flex-between">
-            <div class="depiction">
-              {{ $t("attributes.description.dropdown.divided") }}
-            </div>
-            <LabelAndSelect
-              label=""
-              :disabled="hidden == 'true'"
-              v-model="divided"
-            />
-          </div>
-        </div>
-        <div class="attribute">
-          <h3>Hidden</h3>
-          <div class="flex-between">
-            <div class="depiction">
-              {{ $t("attributes.description.dropdown.hidden") }}
-            </div>
-            <LabelAndSelect label="" v-model="hidden" />
-          </div>
-        </div>
-      </div>
-
-      <div class="demo-app" :class="[appStatus, { hidden: toBoolean(hidden) }]">
-        <h6>
-          *{{ $t("ui.page.sentence.attribute_only_change_first_dropdown") }}
-        </h6>
-        <GeneralDemo
-          class="demo-app"
-          :autoFocus="true"
-          :displayRefreshBtn="true"
-          :displayShowCodeBtn="false"
-          :dropdown="{
-            displayAll: toBoolean(displayAll),
-            disabled: toBoolean(disabled),
-            divided: toBoolean(divided),
-            hidden: toBoolean(hidden),
-          }"
-        />
-      </div>
-    </div>
+    <Other />
   </div>
 </template>
 
 <script>
 import Value from "./value.vue";
 import Custom from "./custom.vue";
-import LabelAndSelect from "../tools/label-and-select.vue";
-import GeneralDemo from "../tools/general-demo.vue";
+import Other from "./other.vue";
 
 export default {
   name: "dropdown-slot",
   components: {
     Value,
     Custom,
-    LabelAndSelect,
-    GeneralDemo,
-  },
-  data() {
-    return {
-      displayAll: "false",
-      disabled: "false",
-      divided: "false",
-      hidden: "false",
-      appStatus: [],
-    };
-  },
-  methods: {
-    toBoolean(value) {
-      return value == "false" ? false : true;
-    },
+    Other,
   },
 };
 </script>
-<style scoped lang="scss">
-.flex-between {
-  .attributes {
-    width: 52%;
-  }
-  .demo-app {
-    width: 40%;
-  }
-  @media all and (max-width: 1200px) {
-    .demo-app {
-      width: 30%;
-    }
-  }
-  @media all and (max-width: 992px) {
-    .demo-app {
-      width: 40%;
-    }
-    .demo-app {
-      margin-top: 24px;
-    }
-  }
-  @media all and (max-width: 576px) {
-    & {
-      flex-direction: column;
-    }
-    .attributes,
-    .demo-app {
-      width: 100%;
-    }
-    .demo-app {
-      margin-top: 24px;
 
-      &.selecting.hidden {
-        margin-bottom: 30px;
-      }
-      &.selecting {
-        margin-bottom: 150px;
-      }
-    }
-  }
-}
-</style>
---
+++ demo/components/dropdown/other.vue
@@ -0,0 +1,117 @@
+<template>
+  <div id="dropdown-other-attributes" class="demo">
+    <div class="flex-between to6-4">
+      <div class="attributes">
+        <div class="attribute">
+          <h3>DisplayAll</h3>
+          <div class="flex-between">
+            <div class="depiction">
+              {{ $t("attributes.description.dropdown.display-all") }}
+            </div>
+            <LabelAndSelect
+              label=""
+              v-model="attributes.displayAll"
+              :disabled="attributes.hidden == 'true'"
+            />
+          </div>
+        </div>
+        <div class="attribute">
+          <h3>Disabled</h3>
+          <div class="flex-between">
+            <div class="depiction">
+              {{ $t("attributes.description.dropdown.disabled") }}
+            </div>
+            <LabelAndSelect
+              label=""
+              :disabled="attributes.hidden == 'true'"
+              v-model="attributes.disabled"
+            />
+          </div>
+        </div>
+        <div class="attribute">
+          <h3>Divided</h3>
+          <div class="flex-between">
+            <div class="depiction">
+              {{ $t("attributes.description.dropdown.divided") }}
+            </div>
+            <LabelAndSelect
+              label=""
+              :disabled="attributes.hidden == 'true'"
+              v-model="attributes.divided"
+            />
+          </div>
+        </div>
+        <div class="attribute">
+          <h3>Hidden</h3>
+          <div class="flex-between">
+            <div class="depiction">
+              {{ $t("attributes.description.dropdown.hidden") }}
+            </div>
+            <LabelAndSelect label="" v-model="attributes.hidden" />
+          </div>
+        </div>
+
+        <ALineCode :bolders="bolders" />
+      </div>
+
+      <div class="demo-app">
+        <h6>
+          *{{ $t("ui.page.sentence.attribute_only_change_first_dropdown") }}
+        </h6>
+        <GeneralDemo
+          :autoFocus="true"
+          :displayRefreshBtn="true"
+          :displayShowCodeBtn="false"
+          :dropdown="{
+            country: {
+              displayAll: toBoolean(attributes.displayAll),
+              disabled: toBoolean(attributes.disabled),
+              divided: toBoolean(attributes.divided),
+              hidden: toBoolean(attributes.hidden),
+            },
+          }"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LabelAndSelect from "../tools/label-and-select.vue";
+import GeneralDemo from "../tools/general-demo.vue";
+import ALineCode from "./tools/a-line-code.vue";
+
+export default {
+  name: "dropdown-other-attributes",
+  components: {
+    LabelAndSelect,
+    GeneralDemo,
+    ALineCode,
+  },
+  data() {
+    return {
+      attributes: {
+        displayAll: "false",
+        disabled: "false",
+        divided: "false",
+        hidden: "false",
+      }
+    };
+  },
+  computed: {
+    bolders() {
+      return Object.entries(this.attributes)
+        .filter(([key, value]) => key != "" && value === "true")
+        .map(([key]) => key);
+    },
+  },
+  methods: {
+    toBoolean(value) {
+      return value == "false" ? false : true;
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+
+</style>
