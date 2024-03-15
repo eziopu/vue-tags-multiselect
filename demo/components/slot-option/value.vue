c20ffa895728f9853d56e8e363d5a646caf4cb41
eziopu <atkdet@gmail.com>
2024-03-15 15:50:59 +0800

    refactor value 排版

--- demo/components/slot-option/value.vue
+++ demo/components/slot-option/value.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="option-value" class="demo">
     <h3>Value</h3>
-    <div class="flex-between to6-4">
+    <div class="flex-between to5-5">
       <div class="depiction">
       {{ $t("attributes.description.option.value") }}
       *{{ $t("attributes.description.option.value__notice") }}
