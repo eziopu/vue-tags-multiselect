9b097f28ce030d99eb0f03a78626cefc4b3f92f6
eziopu <atkdet@gmail.com>
2024-03-15 11:41:51 +0800

    refactor value demo

--- demo/components/slot-option/tools/a-line-code.vue
+++ demo/components/slot-option/tools/a-line-code.vue
@@ -1,25 +1,43 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
-  <pre><code class="tag prettyprint lang-html customize"><!--
-  -->&lt;v-tag-dropdown value="country"&gt;
-    <span v-show="showTitle"><!--  
-    -->&lt;v-tag-option <span :class="{ bolder: bolder.includes('title') }">title</span>&gt;
-      &lt;i class="fa fa-flag"&gt;&lt;/i&gt; <span v-html="renderData('general.Country')"></span>
-    &lt;/v-tag-option&gt;
-    </span><!--
-    -->&lt;v-tag-option <span :class="{ bolder: bolder.includes('value') }">value</span>="mare"<!--
-      --><span class="bolder" v-if="bolder.includes('selected')"> selected</span><!--
-      --><span class="bolder" v-if="bolder.includes('displayValue')"> display-value </span><!--
-      --><span v-if="bolder.includes('displayValue')"><br/>   // or <span class="bolder">:displayValue="true"</span><br/>  </span><!--
-      -->&gt;<span v-html="renderData('data.country.Māre')"></span>&lt;/v-tag-option&gt;
-    &lt;v-tag-option <span :class="{ bolder: bolder.includes('value') }">value</span>="eldia"&gt;<span v-html="renderData('data.country.Eldia')"></span>&lt;/v-tag-option&gt;
-  &lt;/v-tag-dropdown&gt;
-  </code></pre>
+<pre class="tag prettyprint lang-html customize"><code><!--
+  -->&lt;v-tags-multiselect<br><!--
+  --><Space />v-model="result"<br><!--
+  -->&gt;<br><!--
+  --><Space />&lt;v-tag-dropdown value="country"&gt;<br><!--
+    --><span v-show="showTitle"><!--
+      --><Space :n="2"/>&lt;v-tag-option <span :class="{ bolder: bolders.includes('title') }">title</span>&gt;<br><!--
+        --><Space :n="4"/>&lt;i class="fa fa-flag"&gt;&lt;/i&gt; <span v-html="renderData('general.Country')"></span><br><!--
+      --><Space :n="2"/>&lt;/v-tag-option&gt;<br><!--
+    --></span><!--
+
+    --><Space :n="2"/>&lt;v-tag-option<!--
+      --><span :class="{ bolder: bolders.includes('value') }">value</span>="mare"<!--
+      --><span class="bolder" v-if="bolders.includes('selected')"> selected</span><!--
+      --><span class="bolder" v-if="bolders.includes('displayValue')"> display-value</span><!--
+      --><span v-if="bolders.includes('displayValue')"><br/>   // or <span class="bolder">:displayValue="true"</span><br/>  </span><!--
+      -->&gt;<br><!--
+      --><Space :n="4"/><span v-html="renderData('data.country.Māre')"></span><br><!--
+    --><Space :n="2"/>&lt;/v-tag-option&gt;<br><!--
+
+    --><Space :n="2"/>&lt;v-tag-option <span :class="{ bolder: bolders.includes('value') }">value</span>="eldia"&gt;<br><!--
+      --><Space :n="4"/><span v-html="renderData('data.country.Eldia')"></span><br><!--
+    --><Space :n="2"/>&lt;/v-tag-option&gt;<br><!--
+
+    --><Space />&lt;/v-tag-dropdown&gt;<br><!--
+    --><Space :n="2" />...<br><!--
+  -->&lt;/v-tags-multiselect&gt;<!--
+--></code></pre>
 </template>
 
 <script>
+import Space from "../../tools/show-html-code/space.vue";
+
 export default {
   name: "option-tools-line-code",
+  components: {
+    Space,
+  },
   props: {
     showTitle: {
       type: Boolean,
@@ -27,10 +45,10 @@ export default {
         return true;
       },
     },
-    bolder: {
-      type: String,
+    bolders: {
+      type: Array,
       default: () => {
-        return "";
+        return [];
       },
     },
   },
@@ -41,12 +59,3 @@ export default {
   },
 };
 </script>
-<style scoped lang="scss">
-pre {
-  padding: 6px !important;
-
-  span {
-    line-height: 1.6;
-  }
-}
-</style>
--- demo/components/slot-option/value.vue
+++ demo/components/slot-option/value.vue
@@ -1,25 +1,17 @@
 <template>
   <div id="option-value" class="demo">
-    <h4>Value</h4>
-    <div class="depiction">
+    <h3>Value</h3>
+    <div class="flex-between to6-4">
+      <div class="depiction">
       {{ $t("attributes.description.option.value") }}
       *{{ $t("attributes.description.option.value__notice") }}
-    </div>
-
-    <div class="flex-between">
-      <ALineCode bolder="value" class="a-line-code" :showTitle="false" />
-
-      <div class="demo-app" :class="appStatus">
-        <FocusDemoApp
-          v-model="appStatus"
-          :dropdown="{
-            hiddenName: true,
-          }"
-          :option="{
-            title: false,
-          }"
-        >
-        </FocusDemoApp>
+        <br />
+        <ALineCode :bolders="['value']" />
+      </div>
+      <div>
+        result is object array 
+        <br />
+        <pre><code>{ country: ["mare", "eldia"], ...}</code></pre>
       </div>
     </div>
   </div>
@@ -27,21 +19,11 @@
 
 <script>
 import ALineCode from "./tools/a-line-code.vue";
-import FocusDemoApp from "../tools/focus-demo-app.vue";
 
 export default {
   name: "option-value",
   components: {
     ALineCode,
-    FocusDemoApp,
-  },
-  data() {
-    return {
-      appStatus: [],
-    };
   },
 };
 </script>
-<style scoped lang="scss">
-@import "./assets/stylesheets.scss";
-</style>
