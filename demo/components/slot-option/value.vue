2df804f7cb416ee81c40c11bfe8d7b13b8745ab2
eziopu <atkdet@gmail.com>
2024-03-20 00:41:20 +0800

    refactor value demo

--- demo/components/slot-option/value.vue
+++ demo/components/slot-option/value.vue
@@ -1,29 +1,114 @@
+<script>
+export default {
+  name: "option-value",
+};
+</script>
+
+<script setup>
+import ALineCode from "./tools/a-line-code.vue";
+import GeneralDemo from "../tools/general-demo.vue";
+
+const i18n = "attributes.description.option";
+
+import { ref, inject } from "vue";
+const firstDemoStatus = ref([]);
+const secondDemoStatus = ref([]);
+
+const framework = inject("framework");
+</script>
+
 <template>
   <div id="option-value" class="demo">
     <h3>Value</h3>
     <div class="flex-between to5-5">
       <div class="depiction">
-      {{ $t("attributes.description.option.value") }}
-      *{{ $t("attributes.description.option.value__notice") }}
+      {{ $t(`${i18n}.value`) }}
+      *{{ $t(`${i18n}.value__notice`) }}
+
+       <div class="sub-depiction">
+        <p
+          v-for="index in [0,1,2]"
+          :key="index"
+          v-html="$t(`${i18n}.value__notices.${index}`)"
+        />
+      </div>
+
         <br />
         <ALineCode :bolders="['value']" />
       </div>
+
       <div>
-        result is object array 
-        <br />
-        <pre><code>{ country: ["mare", "eldia"], ...}</code></pre>
+        <div>
+          result is object array 
+          <br />
+          <pre><code>{ country: ["mare", "eldia"], ...}</code></pre>
+        </div>
+
+        <div class="general-demos" :class="framework">
+          <div class="general-demo__first" :class="firstDemoStatus">
+            <h4>{{ $t(`ui.general.filled`) }}</h4>
+            <GeneralDemo
+              v-model="firstDemoStatus"
+              :key="`filled`"
+              :autoFocus="true"
+              :displayOutput="true"
+              :displayRefreshBtn="true"
+              :displayShowCodeBtn="false"
+              :dropdown="{
+                country: {isDisplayForDemo: true, displayAll: true},
+                name: {isDisplayForDemo: false},
+                remark: {isDisplayForDemo: false}
+              }"
+            />
+          </div>
+          <div class="general-demo__second" :class="secondDemoStatus">
+            <h4>{{ $t(`ui.general.not_filled`) }}</h4>
+            <GeneralDemo
+              v-model="secondDemoStatus"
+              :key="`not_filled`"
+              :autoFocus="true"
+              :displayOutput="true"
+              :displayRefreshBtn="true"
+              :displayShowCodeBtn="false"
+              :dropdown="{
+                country: {isDisplayForDemo: true, displayAll: true},
+                name: {isDisplayForDemo: false},
+                remark: {isDisplayForDemo: false}
+              }"
+              :option="{
+                country: [{}, {value: ''}, {value: ''}],
+              }"
+            />
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
 </template>
 
-<script>
-import ALineCode from "./tools/a-line-code.vue";
+<style scoped lang="scss">
+.general-demos {
+  margin-top: 1rem;
 
-export default {
-  name: "option-value",
-  components: {
-    ALineCode,
-  },
-};
-</script>
+  .general-demo__first,
+  .general-demo__second {
+    transition: all 0.2s ease-out;
+    margin-bottom: 3rem;
+  }
+  .general-demo__first.selecting {
+    margin-bottom: 10rem;
+  }
+  
+  .general-demo__second.selecting {
+    margin-bottom: 8rem;
+  }
+}
+
+.general-demos.default {
+  .general-demo__first.selecting {
+    margin-bottom: 8rem;
+  }
+}
+</style>
+
