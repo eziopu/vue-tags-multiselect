12a244f777083c250be72c83f86526029e37fdda
eziopu <atkdet@gmail.com>
2024-06-26 13:19:57 +0800

    refactor tool label-and-select <script setup>, computed options

--- demo/components/play-helps/attributes/label-and-select.vue
+++ demo/components/play-helps/attributes/label-and-select.vue
@@ -1,39 +1,40 @@
+
+<script>
+export default {
+  name: "label-and-select",
+};
+</script>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  value: {
+    type: String,
+    default: "",
+  },
+  values: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+const options = computed(() => {
+  return props.values.length > 0 ? props.values : ["true", "false"];
+});
+</script>
+
 <template>
   <div class="flex-between">
     <div>{{ value }}</div>
     <select v-model="app[value]" :disabled="disabled">
-      <option v-for="item in values" :key="item">
+      <option v-for="item in options" :key="item">
         {{ item }}
       </option>
     </select>
   </div>
 </template>
-
-<script>
-import InjectApp from "../mixins/inject-app.js";
-import { defineComponent } from "vue";
-export default defineComponent({
-  name: "label-and-select",
-  mixins: [InjectApp],
-  props: {
-    disabled: {
-      type: Boolean,
-      default: () => {
-        return false;
-      },
-    },
-    value: {
-      type: String,
-      default: () => {
-        return "";
-      },
-    },
-    values: {
-      type: Array,
-      default: () => {
-        return ["true", "false"];
-      },
-    },
-  },
-});
-</script>
