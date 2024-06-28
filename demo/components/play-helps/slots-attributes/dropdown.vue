c2b0d42f2daabb853e994073ed14c8fca9e737e8
eziopu <atkdet@gmail.com>
2024-06-28 15:05:14 +0800

    refactor slots-dropdown ues table-template

--- demo/components/play-helps/slots-attributes/dropdown.vue
+++ demo/components/play-helps/slots-attributes/dropdown.vue
@@ -7,9 +7,9 @@ export default {
 import { inject } from 'vue'
 import { GET_PACKAGE_V_DROPDOWN_PROPS_DETAIL } from '../../models.js'
 import HelpTitle from '../components/title.vue'
+import TableTemplate from './table-template.vue'
 
 const displays = inject('displays')
-const attributes = GET_PACKAGE_V_DROPDOWN_PROPS_DETAIL()
 </script>
 
 <template>
@@ -17,66 +17,13 @@ const attributes = GET_PACKAGE_V_DROPDOWN_PROPS_DETAIL()
     <HelpTitle label="Slot Dropdown Attributes" target="slotDropdown" />
 
     <transition name="slide">
-      <div class="detail attributes container" v-if="displays.slotDropdown">
-        <div class="row titles">
-          <div
-            v-for="value in ['Attribute', 'Description', 'Type', 'Default', 'Required']"
-            :key="value"
-          >
-            <h4>{{ $t(`ui.general.${value}`) }}</h4>
-          </div>
-        </div>
-
-        <div class="row" v-for="(attribute, key) in attributes" :key="key">
-          <div>
-            <div>{{ $toKebabCase(key) }}</div>
-          </div>
-          <div>
-            <span class="i-block d-md-none">{{ $t(`ui.general.Description`) }}: </span>
-            <span>{{ $t(`attributes.dropdown.${$toKebabCase(key)}`) }}</span>
-          </div>
-          <div>
-            <span class="i-block d-md-none">{{ $t(`ui.general.Type`) }}: </span>
-            <span>{{ attribute.type }}</span>
-          </div>
-          <div>
-            <span class="i-block d-md-none">{{ $t(`ui.general.Default`) }}: </span>
-            <span>{{ attribute.default }}</span>
-          </div>
-          <div v-if="key == 'value'">
-            <span class="i-block d-md-none">{{ $t(`ui.general.Required`) }}: </span>
-            *yes
-          </div>
-          <div v-else class="d-none d-md-block">no</div>
-        </div>
-      </div>
+      <TableTemplate
+        v-if="displays.slotDropdown"
+        :attributes="GET_PACKAGE_V_DROPDOWN_PROPS_DETAIL()"
+        :i18n="`attributes.dropdown`"
+      >
+      </TableTemplate>
     </transition>
   </div>
 </template>
 
-<style scoped lang="scss">
-@media all and (min-width: 576px) {
-  .row {
-    & > div:nth-child(1) {
-      width: 20%;
-      order: 1;
-    }
-    & > div:nth-child(2) {
-      width: 45%;
-      order: 2;
-    }
-    & > div:nth-child(3) {
-      width: 10%;
-      order: 3;
-    }
-    & > div:nth-child(4) {
-      width: 10%;
-      order: 4;
-    }
-    & > div:nth-child(5) {
-      width: 15%;
-      order: 5;
-    }
-  }
-}
-</style>
