074d2e4e56d22437297fcac9661cc002b4ad394f
eziopu <atkdet@gmail.com>
2024-07-06 19:37:00 +0800

    refactor tooltip to share

--- demo/components/play-helps/events/components/value.vue
+++ demo/components/play-helps/events/components/value.vue
@@ -29,9 +29,9 @@ const appAttributes = inject('attributes')
     <template v-else>
       <template v-if="keyName == 'clear' && appAttributes.clearable.value == false">
         not enabled
-        <div class="attribute-tooltip">
+        <div class="tool-tooltip">
           <i class="fa fa-info-circle" aria-hidden="true"></i>
-          <span class="attribute-tooltip__text">Attribute clearable is false</span>
+          <span class="tool-tooltip__text">Attribute clearable is false</span>
         </div>
       </template>
       <template v-else>
@@ -69,32 +69,5 @@ pre.data {
       }
     }
   }
-}
-
-/* 
-  Source: https://www.w3schools.com/css/css_tooltip.asp
-  Description: Tooltip
-*/
-
-.attribute-tooltip {
-  position: relative;
-  display: inline-block;
-}
-
-.attribute-tooltip .attribute-tooltip__text {
-  visibility: hidden;
-  width: 120px;
-  background-color: black;
-  color: #fff;
-  text-align: center;
-  border-radius: 6px;
-  padding: 5px 0;
-
-  position: absolute;
-  z-index: 1;
-}
-
-.attribute-tooltip:hover .attribute-tooltip__text {
-  visibility: visible;
 }
 </style>
