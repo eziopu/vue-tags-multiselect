3fbe0a5783afe753ef92e71a4822cde0b81499d0
eziopu <atkdet@gmail.com>
2024-07-22 17:34:06 +0800

    refactor loadings and placeholders attribute use <script setup>

--- demo/components/app/dropdown-loading.vue
+++ demo/components/app/dropdown-loading.vue
@@ -1,3 +1,18 @@
+<script>
+export default {
+  name: 'app-dropdown-loading'
+}
+</script>
+
+<script setup>
+import { inject, computed } from 'vue'
+import { SLOTS } from '@models/attributes/default.js'
+
+const getLoadingAttributes = inject('getLoadingAttributes')
+
+const attributes = computed(() => getLoadingAttributes())
+</script>
+
 <template>
   <div id="dropdown-loading" class="demo">
     <ToolTitle>Dropdown loading</ToolTitle>
@@ -5,39 +20,34 @@
     <div class="depiction">
       {{ $t(`attributes.app.dropdownLoading`) }}
       <div class="sub-depiction">
-        <p
-          v-html="$t(`attributes.slots.dropdown-loading__notice`)"
-        ></p>
+        <p v-html="$t(`attributes.slots.dropdown-loading__notice`)"></p>
       </div>
     </div>
 
     <div class="attributes">
-      <LabelAndControls
-        label="dropdownLoading"
-        v-model="attributes.dropdownLoading"
-        :disabled="attributes.loading == 'true'"
-      />
+      <div style="display: flex; align-items: center">
+        <LabelAndControls
+          label="dropdownLoading"
+          v-model="attributes.dropdownLoading"
+          :disabled="attributes.loading == true"
+        />
+        <span
+          style="padding-left: 12px"
+          v-html="$t(`ui.general.defaultIs__key`, { key: String('false') })"
+        ></span>
+      </div>
 
       <LabelAndControls
         model="input"
         class="flex-column"
         label="dropdown loading icon slot"
         v-model="attributes.dropdownLoadingContent"
-      />
-
+      >
+        <template v-slot:fake-placeholder>
+          default :
+          <div v-html="SLOTS.dropdownLoading"></div>
+        </template>
+      </LabelAndControls>
     </div>
   </div>
 </template>
-
-<script>
-
-export default {
-  name: "app-dropdown-loading",
-  inject: ["getLoadingAttributes"],
-  computed: {
-    attributes() {
-      return this.getLoadingAttributes();
-    },
-  },
-};
-</script>
--- demo/components/app/loading.vue
+++ demo/components/app/loading.vue
@@ -1,3 +1,18 @@
+<script>
+export default {
+  name: 'app-loading'
+}
+</script>
+
+<script setup>
+import { inject, computed } from 'vue'
+import { ATTRIBUTES, SLOTS } from '@models/attributes/default.js'
+
+const getLoadingAttributes = inject('getLoadingAttributes')
+
+const attributes = computed(() => getLoadingAttributes())
+</script>
+
 <template>
   <div id="loading" class="demo">
     <ToolTitle>Loading</ToolTitle>
@@ -7,32 +22,31 @@
     </div>
 
     <div class="attributes">
-      <LabelAndControls label="loading" v-model="attributes.loading" />
+      <div style="display: flex; align-items: center">
+        <LabelAndControls label="loading" v-model="attributes.loading" />
+        <span
+          style="padding-left: 12px"
+          v-html="$t(`ui.general.defaultIs__key`, { key: String('false') })"
+        ></span>
+      </div>
       <LabelAndControls
         model="input"
         class="flex-column"
         label="loading placeholder"
         v-model="attributes.loadingPlaceholder"
-        placeholder="default: Wait a moment, please."
+        :placeholder="`default: ${ATTRIBUTES.placeholders.loading}`"
       />
       <LabelAndControls
         model="input"
         class="flex-column"
         label="loading icon slot"
         v-model="attributes.loadingContent"
-      />
+      >
+        <template v-slot:fake-placeholder>
+          default :
+          <div v-html="SLOTS.loading"></div>
+        </template>
+      </LabelAndControls>
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  name: "app-loading",
-  inject: ["getLoadingAttributes"],
-  computed: {
-    attributes() {
-      return this.getLoadingAttributes();
-    },
-  },
-};
-</script>
