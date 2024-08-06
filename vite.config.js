531efba8a7f692dc944dbebc09093afc646ae0fc
eziopu <atkdet@gmail.com>
2024-08-06 10:41:42 +0800

    chore vite config build add exports: 'named'

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,28 @@
-import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
 
-import vue from "@vitejs/plugin-vue";
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   build: {
     lib: {
-      entry: "src/index.js",
-      name: "vue-tags-multiselect",
-      fileName: "vue-tags-multiselect",
+      entry: 'src/index.js',
+      name: 'vue-tags-multiselect',
+      fileName: 'vue-tags-multiselect',
     },
     rollupOptions: {
-      external: ["vue"],
+      external: ['vue'],
       output: {
+        exports: 'named',
         globals: {
-          vue: "Vue"
+          vue: 'Vue'
         },
       },
     },
