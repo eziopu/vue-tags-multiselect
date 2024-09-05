aff2c7042d233b07c8fd77d81e16220fd3fb8f5e
eziopu <atkdet@gmail.com>
2024-09-05 17:14:57 +0800

    refactor app slot v-dropdown, props models

---
+++ src/components/slots/models.js
@@ -0,0 +1,22 @@
+export function vDropdownProps() {
+  return {
+    disabled: false,
+    divided: false,
+    displayAll: false,
+    hidden: false,
+    custom: false,
+    system: false,
+    value: "",
+  };
+}
+
+export function vOptionProps() {
+  return {
+    title: false,
+    displayValue: false,
+    disabled: false,
+    divided: false,
+    selected: false,
+    value: "",
+  };
+}
