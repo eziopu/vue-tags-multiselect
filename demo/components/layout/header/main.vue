c555642f4cd5ed32d8a9c66a5262100a49a41971
eziopu <atkdet@gmail.com>
2024-09-11 14:33:38 +0800

    fix demo page navbar home a link add framework

--- demo/components/layout/header/main.vue
+++ demo/components/layout/header/main.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="navbar">
-    <a href="/" class="navbar-site">
-      <img class="logo" src="../../../assets/logo.svg" alt="Vue">
+    <a :href="home_link" class="navbar-site">
+      <img class="logo" src="../../../assets/logo.svg" alt="Vue" />
       <h1 class="site-name">vue-tags-multiselect</h1>
     </a>
 
@@ -11,10 +11,12 @@
       <Frameworks />
       <Languages />
       <colorTheme />
-      <a class="navbar-item navbar-icon navbar-github"
+      <a
+        class="navbar-item navbar-icon navbar-github"
         href="https://github.com/eziopu/vue-tags-multiselect"
         target="_blank"
-        title="open github">
+        title="open github"
+      >
         <div class="navbar-item--github">
           <i class="fa fa-github"></i>
         </div>
@@ -23,22 +25,24 @@
 
     <Burger />
     <div class="header-overlay"></div>
-
   </header>
 </template>
 
 <script>
-  export default {
-    name: "header-main",
-  }
+export default {
+  name: 'header-main'
+}
 </script>
 
 <script setup>
-import Pages from "./pages.vue";
-import Frameworks from "./frameworks.vue";
-import Languages from "./languages.vue";
-import colorTheme from "./colorTheme.vue";
-import Burger from "./burger.vue";
+import Pages from './pages.vue'
+import Frameworks from './frameworks.vue'
+import Languages from './languages.vue'
+import colorTheme from './colorTheme.vue'
+import Burger from './burger.vue'
 
+import { inject } from 'vue'
+
+const framework = inject('framework')
+const home_link = framework.value !== 'default' ? `#/?framework=${framework.value}` : '#/'
 </script>
-
